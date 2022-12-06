1ee064dc79beaf5873784ee45a7449814f282a49
Joshua Haddad <joshuaihaddad@gmail.com>
2022-12-06 10:33:23 -0600

    Checking out some benchmarking from @Bryson14

--- Day6/src/main.rs
+++ Day6/src/main.rs
@@ -18,11 +18,50 @@ fn get_start_of_packet(buffer: &String, n_chars: usize) -> usize{
     }
     return 0 as usize;
 }
+
+// Grabbing some code from @Bryson14 https://github.com/Bryson14/RustLangFun/blob/main/advent_of_code_2022/src/day6.rs to check benchmarks for different implementations    
+fn hashset_iter_check(s: &String, no_repeat_len: usize) -> usize {
+    use std::collections::HashSet;
+    let mut marker = no_repeat_len;
+    for i in no_repeat_len..s.len() {
+        let mut set: HashSet<char> = HashSet::new();
+        s[i - no_repeat_len..i].chars().for_each(|c| {
+            let _ = set.insert(c);
+        });
+
+        // if the hashset has all elements and there were no repeated characters,
+        // then all elements were
+        if set.len() < no_repeat_len {
+            marker += 1;
+        } else {
+            return marker;
+        }
+    }
+    marker
+}
+
+fn benchmark(func: &dyn Fn(&String, usize) -> usize, iterations: u32, buffer: usize, data: &str) {
+    use std::time::Instant;
+    let now = Instant::now();
+
+    // Code block to measure.
+    for _ in 0..iterations {
+        let _ = func(&data.to_string(), buffer);
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.3?}", elapsed);
+    println!("Time per call: {:.4?}", elapsed / iterations)
+}
+
 fn main() {
     let buffer = fs::read_to_string("./src/input.txt").expect("Error while reading file");
-    let i = get_start_of_packet(&buffer, 4 as usize);
-    let j = get_start_of_packet(&buffer, 14 as usize);
-    println!("Problem 1 {} \nProblem 2 {}", i, j);
+
+    benchmark(&hashset_iter_check, 1000, 4, &buffer);
+    benchmark(&get_start_of_packet, 1000, 4, &buffer);
+    //let i = get_start_of_packet(&buffer, 4 as usize);
+    //let j = get_start_of_packet(&buffer, 14 as usize);
+    //println!("Problem 1 {} \nProblem 2 {}", i, j);
 }
 
 #[test]
