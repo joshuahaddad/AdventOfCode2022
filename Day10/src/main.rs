2966e9db50a1385f86f532dbb639fb12b441f894
Joshua Haddad <joshuaihaddad@gmail.com>
2022-12-10 01:04:46 -0600

    Solve Day10.2

--- Day10/src/main.rs
+++ Day10/src/main.rs
@@ -3,7 +3,8 @@ use std::fs;
 struct CPU {
     cycles: i32,
     x: i32,
-    signal_str: Vec<i32>
+    signal_str: Vec<i32>,
+    sprite_pos: [i32;3]
 }
 
 impl CPU {
@@ -11,7 +12,8 @@ impl CPU {
         CPU {
             cycles: 0,
             x: 1,
-            signal_str: vec![]
+            signal_str: vec![],
+            sprite_pos: [0, 1, 2]
         }
     }
 }
@@ -19,6 +21,7 @@ impl CPU {
 fn execute_instrs(instructions: &String) -> i32{
     let mut cpu = CPU::new();
     let mut sum_s = 0;
+    let mut crt_line = "".to_string();
     for line in instructions.lines() {
         //println!("{}", line);
         let instr = &line[0..4];
@@ -31,7 +34,20 @@ fn execute_instrs(instructions: &String) -> i32{
 
         for cycle in 0..cycles{
             cpu.cycles += 1;
+            let crt_pixel = cpu.cycles-1;
+            //CRT renders at the start
+            if cpu.sprite_pos.contains(&(crt_pixel % 40)) {
+                crt_line.push('#');
+            }
+            else {
+                crt_line.push('.')
+            }
+            if crt_line.len() == 40 {
+                println!("{}", crt_line);
+                crt_line = "".to_string();
+            }
 
+            // End of Cycle
             if cpu.cycles == 20 || cpu.cycles >= 60 && cpu.cycles % 40 == 20 {
                 cpu.signal_str.push(cpu.x*cpu.cycles);
                 sum_s += cpu.x*cpu.cycles;
@@ -41,6 +57,7 @@ fn execute_instrs(instructions: &String) -> i32{
             if add_instr && cycle == 1 {
                 let arg: i32 = line[5..line.len()].parse().unwrap_or(0);
                 cpu.x += arg;
+                cpu.sprite_pos = [cpu.x-1, cpu.x, cpu.x+1];
             }         
         }
     }
