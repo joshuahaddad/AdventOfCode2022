e0a09c9a0a5a22d55e320f55871002461606855e
Joshua Haddad <joshuaihaddad@gmail.com>
2022-12-15 13:24:11 -0600

    Solve day 14.2! Not too bad of a day

--- Day14/src/main.rs
+++ Day14/src/main.rs
@@ -32,12 +32,11 @@ fn generate_rocks(scan: String) -> (HashSet<(usize, usize)>, usize){
     return (rocks, max_y);
 }
 
-fn drop_sand(rocks: &mut HashSet<(usize, usize)>, gen: (usize, usize), void: usize) -> bool{
+fn drop_sand(rocks: &mut HashSet<(usize, usize)>, gen: (usize, usize), void: usize, prob2: bool) -> bool{
     
-    let mut sand = (gen.0, gen.1+1);
-    let mut rest = rocks.contains(&sand);
+    let mut sand = (gen.0, gen.1);
+    let mut rest = false;
     while !rest {
-
         // Check if the vertical position contains a rock
         if rocks.contains(&(sand.0, sand.1+1)){
             // Check if the down-left contains a rock, if it doesnt move the sand
@@ -53,14 +52,27 @@ fn drop_sand(rocks: &mut HashSet<(usize, usize)>, gen: (usize, usize), void: usi
             else {
                 rest = true;
                 rocks.insert(sand);
+
+                // Problem 2 only, if the sand comes to rest at the generator we are done
+                if sand == gen {
+                    return false;
+                }
             }
         }
 
         // If no rock then the sand should fall down
         else {
-            if sand.1+1 > void {
-                //println!("{:?}", sand);
-                return false;
+            if sand.1+1 >= void {
+                
+                // If problem 2 is enabled expand the floor and end
+                if prob2 {
+                    rocks.insert(sand);
+                    rocks.insert((sand.0, void));
+                    return true;
+                }
+                else {
+                    return false;
+                }                
             }
             sand = (sand.0, sand.1+1);
         }    
@@ -70,13 +82,31 @@ fn drop_sand(rocks: &mut HashSet<(usize, usize)>, gen: (usize, usize), void: usi
     return rest;
 }
 
-fn main() {
+fn p1() {
     let scan = fs::read_to_string("src/input.txt").expect("Error reading string");
     let (mut rocks, max_y) = generate_rocks(scan);
     let source: (usize, usize) = (500, 0);
     let mut n = 0;
-    while drop_sand(&mut rocks, source, max_y){
+    while drop_sand(&mut rocks, source, max_y, false){
         n += 1;
     }
-    println!("{:?}", n);
+    println!("Problem 1 {}", n);
+}
+
+fn p2(){
+    let scan = fs::read_to_string("src/input.txt").expect("Error reading string");
+    let (mut rocks, mut max_y) = generate_rocks(scan);
+    let source: (usize, usize) = (500, 0);
+    let mut n = 0;
+    while drop_sand(&mut rocks, source, max_y+2, true){
+        n += 1;
+        
+    }
+
+    // Add one because the loop will stop when the sand ends at the gen
+    println!("Problem 2 {:?}", n+1);
+}
+fn main() {
+    p1();
+    p2();
 }
