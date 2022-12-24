a5b559af6e123f37e9c46fb7afa58a6ac5d3517d
Joshua Haddad <joshuaihaddad@gmail.com>
2022-12-24 17:09:52 -0600

    Optimized with a hashset, may work but slow and runs close to mem limit

--- Day16/src/main.rs
+++ Day16/src/main.rs
@@ -29,66 +29,43 @@ impl fmt::Display for Node {
 }
 
 #[derive(Clone)]
+#[derive(Eq, Hash, PartialEq)]
 struct State {
     pos: String,
-    idx: NodeIndex,
-    elephant: String,
-    ele_idx: NodeIndex,
     time: i32,
-    time_e: i32,
     opened: Vec<(i32, i32)>,
-    unopened: HashSet<String>
+    unopened: Vec<String>
 }
 
 impl State {
-    fn new(start: String, node: NodeIndex, t: i32, non_zero: HashSet<String>, open: Vec<(i32,i32)>) -> State{
+    fn new(start: String, node: NodeIndex, t: i32, non_zero: Vec<String>, open: Vec<(i32,i32)>) -> State{
         State {
             pos: start.clone(),
-            elephant: start.clone(),
-            ele_idx: node,
-            idx: node,
             time: t,
-            time_e: t,
             unopened: non_zero,
             opened: open
         }
     }
     
-    fn move_human(&self, mv: String, node: NodeIndex, t: i32, non_zero: HashSet<String>, open: Vec<(i32,i32)>) -> State{
+    fn mv(&self, mv: String, node: NodeIndex, t: i32, non_zero: Vec<String>, open: Vec<(i32,i32)>) -> State{
         State {
             pos: mv,
-            idx: node,
-            elephant: self.elephant.clone(),
-            ele_idx: self.ele_idx.clone(),
             time: t,
-            time_e: self.time_e,
             unopened: non_zero,
             opened: open
         }
     }
 
-    fn move_ele(&self, mv: String, node: NodeIndex, t: i32, non_zero: HashSet<String>, open: Vec<(i32,i32)>) -> State{
-        State {
-            pos: self.pos.clone(),
-            idx: self.idx.clone(),
-            elephant: mv,
-            ele_idx: node,
-            time: self.time,
-            time_e: t,
-            unopened: non_zero,
-            opened: open
-        }
-    }
 }
 
 
 
 
-fn create_graph(tunnels: &String) -> (Graph::<Node, i32, Directed>, HashMap<String, NodeIndex>, HashSet<String>){
+fn create_graph(tunnels: &String) -> (Graph::<Node, i32, Directed>, HashMap<String, NodeIndex>, Vec<String>){
     let re = Regex::new(r"[\D]+ ([A-Z]+)[\D]+([\d]+)[a-z ;]+([A-Z, ]*)").unwrap();
     let mut g = Graph::<Node, i32, Directed>::new();
     let mut v : HashMap<String, NodeIndex> = HashMap::new();
-    let mut non_zero_v: HashSet<String> = HashSet::new();
+    let mut non_zero_v: Vec<String> = vec![];
     let mut edges: HashMap<NodeIndex, Vec<String>> = HashMap::new();
 
     for cap in re.captures_iter(tunnels){
@@ -102,7 +79,7 @@ fn create_graph(tunnels: &String) -> (Graph::<Node, i32, Directed>, HashMap<Stri
         edges.insert(v_i, paths);
 
         if flow.parse::<i32>().unwrap_or(0) != 0 {
-            non_zero_v.insert(name.to_string());
+            non_zero_v.push(name.to_string());
         } 
 
     }
@@ -121,7 +98,8 @@ fn update_state(initial: &State, to_str: String, to_idx: NodeIndex, t_spent: i32
     let mut unopened = initial.unopened.clone();
     let t = initial.time - t_spent;
     opened.push((pressure, t));
-    unopened.remove(&to_str);
+    let index = unopened.iter().position(|x| *x == to_str).unwrap();
+    unopened.remove(index);
     State::new(to_str, to_idx, t, unopened, opened)
 }
 
@@ -155,6 +133,19 @@ fn iterate_states(g: &Graph::<Node, i32, Directed>, v: &HashMap<String, NodeInde
     return (new_states, finished);
 }
 
+fn get_pressure(opened: Vec<(i32, i32)>, max_p: i32) -> i32{
+    let mut total = 0;
+    for valve in opened{
+        let open_t = valve.1;
+        total += valve.0 * open_t;
+    }
+
+    if total > max_p{
+        return total;
+    }
+
+    return max_p;
+}
 fn try_move(g: &Graph::<Node, i32, Directed>, v: &HashMap<String, NodeIndex>, state: &State, vi: String, plm: &HashMap<NodeIndex, i32>, ele: bool) -> (State, bool) {
     let time_spent = plm[&v[&vi]]+1;
     let time = if ele {state.time_e - time_spent} else {state.time - time_spent};
@@ -169,7 +160,8 @@ fn try_move(g: &Graph::<Node, i32, Directed>, v: &HashMap<String, NodeIndex>, st
         let mut opened = state.opened.clone();
         let mut unopened = state.unopened.clone();
         opened.push((pressure, time));
-        unopened.remove(&vi);
+        let index = unopened.iter().position(|x| *x == vi).unwrap();
+        unopened.remove(index);
         if ele {
             return (state.move_ele(vi, final_dest_idx, time, unopened, opened), false);
         }
@@ -178,17 +170,17 @@ fn try_move(g: &Graph::<Node, i32, Directed>, v: &HashMap<String, NodeIndex>, st
         }
     }
 }
-fn iterate_states_elephant(g: &Graph::<Node, i32, Directed>, v: &HashMap<String, NodeIndex>, states: Vec<State>) -> (Vec<State>,Vec<Vec<(i32,i32)>>){
-    let mut new_states: Vec<State> = vec![];
+fn iterate_states_elephant(g: &Graph::<Node, i32, Directed>, v: &HashMap<String, NodeIndex>, states: HashSet<State>) -> (HashSet<State>, i32){
+    let mut new_states: HashSet<State> = HashSet::new();
     let mut finished: Vec<Vec<(i32,i32)>> = vec![];
-    
+    let mut max_p = 0;
     for state in states {
         let plm_human = dijkstra(g, v[&state.pos], None, |_| 1);
         let plm_ele = dijkstra(g, v[&state.elephant], None, |_| 1);
 
         // No more moves to consider, this state is finished
         if state.unopened.len() == 0  || (state.time_e == 0 && state.time == 0) {
-            finished.push(state.opened.clone());
+            max_p = get_pressure(state.opened, max_p);
         }
 
         //Only one move try and move both the elephant and the human, push to finished
@@ -196,60 +188,53 @@ fn iterate_states_elephant(g: &Graph::<Node, i32, Directed>, v: &HashMap<String,
             let vi = state.unopened.iter().next().unwrap();
             let (ele_s, ele_done) = try_move(g, v, &state, vi.to_string(), &plm_ele, true);
             let (hum_s, hum_done) = try_move(g, v, &state, vi.to_string(), &plm_human, false);
-            if ele_done {finished.push(ele_s.opened);} else {new_states.push(ele_s);}
-            if hum_done {finished.push(hum_s.opened);} else {new_states.push(hum_s);}
+            if ele_done {max_p = get_pressure(ele_s.opened, max_p);} else {new_states.insert(ele_s);}
+            if hum_done {max_p = get_pressure(hum_s.opened, max_p);} else {new_states.insert(hum_s);}
         }
 
         // Elephant has exhaused its time, only move the human
         else if state.time_e == 0 {
             for vi in &state.unopened{
                 let (hum_s, hum_done) = try_move(g, v, &state, vi.to_string(), &plm_human, false);
-                if hum_done {finished.push(hum_s.opened);} else {new_states.push(hum_s);}
+                if hum_done {max_p = get_pressure(hum_s.opened, max_p);} else {new_states.insert(hum_s);}
             }
         }
         // Human has exhausted its time, only move the elephant
         else if state.time == 0 {
             for vi in &state.unopened{
                 let (ele_s, ele_done) = try_move(g, v, &state, vi.to_string(), &plm_ele, true);
-                if ele_done {finished.push(ele_s.opened);} else {new_states.push(ele_s);}
+                if ele_done {max_p = get_pressure(ele_s.opened, max_p);} else {new_states.insert(ele_s);}
             }
         }
     
         else {
-            for pairs in state.unopened.clone().into_iter().combinations(2){
+            for pairs in state.unopened.clone().into_iter().permutations(2){
                 let (v1, v2) = (pairs[0].clone(), pairs[1].clone());
                 let (ele_s, ele_done) = try_move(g, v, &state, v1, &plm_ele, true);
                 let (hum_s, hum_done) = try_move(g, v, &state, v2, &plm_human, false);
-                if ele_done {finished.push(ele_s.opened);} else {new_states.push(ele_s);}
-                if hum_done {finished.push(hum_s.opened);} else {new_states.push(hum_s);}
+                if ele_done {max_p = get_pressure(ele_s.opened, max_p);} else {new_states.insert(ele_s);}
+                if hum_done {max_p = get_pressure(hum_s.opened, max_p);} else {new_states.insert(hum_s);}
             }
         }
         
     }
 
-    return (new_states, finished);
+    return (new_states, max_p);
 }
 fn main() {
     let tunnels = fs::read_to_string("./src/input.txt").expect("Error reading file");
     let (g, v, non_zero) = create_graph(&tunnels);
     let start: String = "AA".to_string();
-    let mut states: Vec<State> = vec![State::new(start.clone(), v[&start], 26, non_zero.clone(), vec![])];
-    let mut end: Vec<Vec<(i32,i32)>> = vec![];
-    let mut finished: Vec<Vec<(i32,i32)>> = vec![]; 
+    let mut states: HashSet<State> = HashSet::new();
+    states.insert(State::new(start.clone(), v[&start], 26, non_zero.clone(), vec![]));
+    let mut iter_max_p = 0;
     let mut max_p = 0;
     while states.len() > 0{
         println!("{}", states.len());
-        (states, finished) = iterate_states_elephant(&g,&v,states);
-        for state in finished {
-            let mut total = 0;
-            for open in state{
-                
-                let open_t = open.1;
-                total += open.0 * open_t;
-            }
-            if total > max_p{
-                max_p = total;
-            }
+        (states, iter_max_p) = iterate_states_elephant(&g,&v,states);
+        println!("Pressure {:?}", iter_max_p);
+        if iter_max_p > max_p{
+            max_p = iter_max_p;
         }
     }
 
